e026d2a882fc2ea9f16d7d9d0cf0ef89ac403710
Manoj kumar <manojkumarpalakuri@gmail.com>
2026-05-08 15:21:27 +0530

    Added Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import SmoothScroll from "@/components/layout/SmoothScroll";
 import AskManoj from "@/components/ui/ask-manoj";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const viewport: Viewport = {
   width: "device-width",
